xen0n/loongson-overlay

commit 34a8eea05e04e18842e755d3cc49bc32069b23e8

sys-libs/glibc: sync with ::gentoo, change naming convention
Signed-off-by: WANG Xuerui <git@xen0n.name>

sys-libs/glibc/glibc-2.35-r101.ebuild
@@ -20,7 +20,7 @@ SLOT="2.2"
 EMULTILIB_PKG="true"
 
 # Gentoo patchset (ignored for live ebuilds)
-PATCH_VER=2
+PATCH_VER=3
 PATCH_DEV=dilfridge
 
 # LoongArch patchset (also ignored for live ebuilds)
@@ -48,7 +48,7 @@ SRC_URI+=" https://gitweb.gentoo.org/proj/locale-gen.git/snapshot/locale-gen-${L
 SRC_URI+=" multilib-bootstrap? ( https://dev.gentoo.org/~dilfridge/distfiles/gcc-multilib-bootstrap-${GCC_BOOTSTRAP_VER}.tar.xz )"
 SRC_URI+=" systemd? ( https://gitweb.gentoo.org/proj/toolchain/glibc-systemd.git/snapshot/glibc-systemd-${GLIBC_SYSTEMD_VER}.tar.gz )"
 
-IUSE="audit caps cet +clone3 compile-locales +crypt custom-cflags doc gd headers-only +multiarch multilib multilib-bootstrap nscd profile selinux +ssp +static-libs suid systemd systemtap test vanilla"
+IUSE="audit caps cet compile-locales +crypt custom-cflags doc gd headers-only +multiarch multilib multilib-bootstrap nscd profile selinux +ssp +static-libs suid systemd systemtap test vanilla"
 
 # Minimum kernel version that glibc requires
 MIN_KERN_VER="3.2.0"
@@ -171,18 +171,10 @@ GENTOO_GLIBC_XFAIL_TESTS="${GENTOO_GLIBC_XFAIL_TESTS:-yes}"
 # The following tests fail due to the Gentoo build system and are thus
 # executed but ignored:
 XFAIL_TEST_LIST=(
-	# 9) Failures of unknown origin
-	tst-latepthread
-
 	# buggy test, assumes /dev/ and /dev/null on a single filesystem
 	# 'mount --bind /dev/null /chroot/dev/null' breaks it.
 	# https://sourceware.org/PR25909
 	tst-support_descriptors
-
-	# Flaky test, known to fail occasionally:
-	# https://sourceware.org/PR19329
-	# https://bugs.gentoo.org/719674#c12
-	tst-stack4
 
 	# The following tests fail only inside portage
 	# https://bugs.gentoo.org/831267
@@ -527,9 +519,11 @@ setup_env() {
 		# a good start into that direction.
 		# Also, if you're crosscompiling, let's assume you know what you are doing.
 		# Hopefully.
+		# Last, we need the settings of the *build* environment, not of the
+		# target environment...
 
-		local current_binutils_path=$(binutils-config -B)
-		local current_gcc_path=$(gcc-config -B)
+		local current_binutils_path=$(env ROOT="${SYSROOT}" binutils-config -B)
+		local current_gcc_path=$(env ROOT="${SYSROOT}" gcc-config -B)
 		einfo "Overriding clang configuration, since it won't work here"
 
 		export CC="${current_gcc_path}/gcc"
@@ -762,6 +756,14 @@ sanity_prechecks() {
 
 	# When we actually have to compile something...
 	if ! just_headers && [[ ${MERGE_TYPE} != "binary" ]] ; then
+		if [[ -d "${ESYSROOT}"/usr/lib/include ]] ; then
+			# bug #833620, bug #643302
+			eerror "Found ${ESYSROOT}/usr/lib/include directory!"
+			eerror "This is known to break glibc's build."
+			eerror "Please backup its contents then remove the directory."
+			die "Found directory (${ESYSROOT}/usr/lib/include) which will break build (bug #833620)!"
+		fi
+
 		if [[ ${CTARGET} == *-linux* ]] ; then
 			local run_kv build_kv want_kv
 
@@ -863,19 +865,8 @@ src_prepare() {
 		einfo "Done."
 	fi
 
-	if [[ ${PV} != 9999* ]] ; then
-		einfo "Applying LoongArch support patchset ${LOONGARCH_PATCH_VER}"
-		eapply "${WORKDIR}/loongarch-${PV}"
-		einfo "Done."
-	fi
-
-	if use clone3 ; then
-		append-cppflags -DGENTOO_USE_CLONE3
-	else
-		# See e.g. bug #827386, bug #819045.
-		elog "Disabling the clone3 syscall for compatibility with older Electron apps."
-		elog "Please re-enable this flag before filing bugs!"
-	fi
+	# TODO: We can drop this once patch is gone from our patchset
+	append-cppflags -DGENTOO_USE_CLONE3
 
 	default
 

sys-libs/glibc/Manifest
@@ -1,6 +1,6 @@
 DIST gcc-multilib-bootstrap-20201208.tar.xz 5528452 BLAKE2B 16699a6e4df5b2f28a21776ae9e3728b26a9ea251f5580aa5349545ad7c9f6145b9cb6a12ca8f5f96b9cb2a3c70b7e66ca702e4c6f083ac00408e0a20a69e613 SHA512 a243f505e17d0a7e144e8713c077582412f61d6cf7f79baa846de4fb77f5e0f27e11c9a785e14624e04ac52287b32164e7995323aa11caef59113ac438254347
 DIST glibc-2.35-loongarch-patches-20220204-1.tar.xz 55836 BLAKE2B 78f0761579c7559988b98a975c6f47014f4dc5e4361fe6d3b635dff371264880ce9dc4b794f78a24f594211ceb1258740276a10313febf7f26ce2cb35b38fa47 SHA512 8721ed1fad1b97d730446eaf75efbcee700eaa8096f73fcb24eff8c0b90d287b1e9d5d6de9959b93899979cf18390056ee3e81892a0b063d8bc6e89b0539195b
-DIST glibc-2.35-patches-2.tar.xz 22864 BLAKE2B eeba106b90d67fb8837be613ac2ca835ccfaabead38bcfa0408838dab2098c0259d620cb70af953714df6939fd74ccc44fb7ddc6b93172df61f9a5bb7f38356b SHA512 9f1dad92303920ec3fe28129a98a318f79e8645f236485510ce743b6f268c39c739a4d400a44753599723abb06ce01f723fd6b3dd7467dcffdf316e5ba1b773e
+DIST glibc-2.35-patches-3.tar.xz 29992 BLAKE2B aaf615d7cd2c2a00b47f3470ce92c080ffd0e7d768bf724d7e3a9ea50f161a550d9ff3c3c0734e3b00afec67d471aa04b74e050b738bf6e669720a646dab89cb SHA512 203726f333fe1d1db41c81c46cf997bdbe02409a38c4e9f8e9274512d121ad1f9412391828af8b129879e9104c58ca0a1b6f7427abfc32a161f33bd48fa44179
 DIST glibc-2.35.tar.xz 18165952 BLAKE2B 623c728884f070cd87ffeb9203f74206197c52405ac9bc44f3dd519a3468b8e8ae2536c883e5d17d94417dbd1e91775de2e674314e4ff7424f9720026d6b7063 SHA512 e7336ce27561be5d7c217832a1136fb327e057bd8d3f92925b35c97e3e9f9e486948b5a1e03e5e4090772ef06437a074d10b82e68f17f1ad8f22077ee39e1b66
 DIST glibc-systemd-20210729.tar.gz 1480 BLAKE2B 37722c7579df782d890e44dbab99c3de52ab466eb9de80d82405e9bb5620bf39ffc8c5f466a435bdb86ef6d36dd7019c0736573916bda6c67d02a2581e0ec979 SHA512 efd75af58b50522c28cdac7abd1fc56555bc1bb042512c90d8340c1ec09c5791b3872a305bf83723252bbde5855b75d958c041083457765c4cfd170732d09238
 DIST locale-gen-2.22.tar.gz 7971 BLAKE2B 2dc66fa69bf51799d0c34459b654fba6998b80a7e322e9b670036c967e269ad921f50195e6e34c4a83c1f0bad191fd5aa3f37defb82271b73acbca07b7e49d08 SHA512 9798b10dbbc792345a7b7a121dec5f4bba9839a8aec010f01a09f3402fd5bf2376f79e03a6a19bc357010db780037a8811c381136ce19be1f1370374906dff38

commit 1d97e26a75feee5a0eb4c07eb2adfbe556b9f9c1

sys-libs/glibc: fix missing applying of LoongArch patches after syncing with upstream
Fixes: 34a8eea ("sys-libs/glibc: sync with ::gentoo, change naming convention")
Signed-off-by: WANG Xuerui <git@xen0n.name>

sys-libs/glibc/glibc-2.35-r101.ebuild
@@ -866,6 +866,12 @@ src_prepare() {
 		einfo "Done."
 	fi
 
+	if [[ ${PV} != 9999* ]] ; then
+		einfo "Applying LoongArch support patchset ${LOONGARCH_PATCH_VER}"
+		eapply "${WORKDIR}/loongarch-${PV}"
+		einfo "Done."
+	fi
+
 	# TODO: We can drop this once patch is gone from our patchset
 	append-cppflags -DGENTOO_USE_CLONE3
 